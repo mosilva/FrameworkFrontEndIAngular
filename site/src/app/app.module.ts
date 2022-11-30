ad7b090af283c5ac41760c9eb652bce07288b6c5
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-11-30 20:28:31 -0300

    Create all componentes

--- site/src/app/app.module.ts
+++ site/src/app/app.module.ts
@@ -3,11 +3,23 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { DestaqueComponent } from './components/destaque/destaque.component';
+import { SobreComponent } from './components/sobre/sobre.component';
+import { EnderecoComponent } from './components/endereco/endereco.component';
+import { ContatoComponent } from './components/contato/contato.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CarouselComponent,
+    DestaqueComponent,
+    SobreComponent,
+    EnderecoComponent,
+    ContatoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
