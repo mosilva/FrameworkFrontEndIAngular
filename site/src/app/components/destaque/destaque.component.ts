4dfd01a66afc6a06e9e14d98a4fd09c216884721
Marcelo Oliveira <marcelo.olisi@gmail.com>
2022-12-07 17:12:21 -0300

    Create model features (destaque)

--- site/src/app/components/destaque/destaque.component.ts
+++ site/src/app/components/destaque/destaque.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { FeraturesSectionsData } from 'src/app/models/features-section-data.model';
 
 @Component({
   selector: 'app-destaque',
   templateUrl: './destaque.component.html',
-  styleUrls: ['./destaque.component.css']
+  styleUrls: ['./destaque.component.css'],
 })
 export class DestaqueComponent {
-
+  @Input() public featuresDataReceive!: FeraturesSectionsData;
 }
